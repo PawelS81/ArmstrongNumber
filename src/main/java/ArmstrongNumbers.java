97b27e8e3ecc8b6c6eacc2babefcc4b406ca7b71
Pawel_Smolarkiewicz <p.smolarkiewicz@gmail.com>
2019-10-19 13:38:14 +0200

    Zrobione

--- src/main/java/ArmstrongNumbers.java
+++ src/main/java/ArmstrongNumbers.java
@@ -1,7 +1,20 @@
 public class ArmstrongNumbers {
 
-    public static boolean isArmstrongNumber(int number) {
-        return false;
-    }
 
+    public static boolean isArmstrongNumber(int number) {
+
+        int digitsNumber = String.valueOf(number).length();
+        int sum = 0;
+        int temp = number;
+        for (int i = 1; i <= digitsNumber; i++) {
+            int digit = temp % 10;
+            temp = temp / 10;
+            sum = sum + (int) Math.pow(digit, digitsNumber);
+        }
+        if (sum == number) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
